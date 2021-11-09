2706d17358607ae72aac7014405ec01220ac7d5d
Tumidanski Justin D <Justin.Tumidanski@dematic.com>
2021-11-09 08:56:47 -0500

    All REST handlers consider OpenTracing span context.

--- atlas.com/nss/shop/resource.go
+++ atlas.com/nss/shop/resource.go
@@ -13,63 +13,79 @@ import (
 )
 
 const (
-	GetShop = "get_shop"
+	getShop = "get_shop"
 )
 
 func InitResource(router *mux.Router, l logrus.FieldLogger, db *gorm.DB) {
 	nsr := router.PathPrefix("/npcs").Subrouter()
-	nsr.HandleFunc("/{npcId}/shop", rest.RetrieveSpan(GetShop, HandleGetShop(l, db))).Methods(http.MethodGet)
+	nsr.HandleFunc("/{npcId}/shop", registerGetShop(l, db)).Methods(http.MethodGet)
 }
 
-func HandleGetShop(fl logrus.FieldLogger, db *gorm.DB) rest.SpanHandler {
-	return func(span opentracing.Span) http.HandlerFunc {
-		return func(w http.ResponseWriter, r *http.Request) {
-			l := fl.WithFields(logrus.Fields{"originator": GetShop, "type": "rest_handler"})
+type npcIdHandler func(npcId uint32) http.HandlerFunc
 
-			npcId, err := strconv.Atoi(mux.Vars(r)["npcId"])
-			if err != nil {
-				l.WithError(err).Errorf("Unable to properly parse npcId from path.")
-				w.WriteHeader(http.StatusBadRequest)
-				return
-			}
+func parseNpcId(l logrus.FieldLogger, next npcIdHandler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		monsterId, err := strconv.Atoi(vars["npcId"])
+		if err != nil {
+			l.WithError(err).Errorf("Error parsing npcId as uint32")
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		next(uint32(monsterId))(w, r)
+	}
+}
 
-			items, err := item.GetByShopId(db)(uint32(npcId))
-			if err != nil {
-				l.WithError(err).Errorf("Unable to get the shops items.")
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
+func registerGetShop(l logrus.FieldLogger, db *gorm.DB) http.HandlerFunc {
+	return rest.RetrieveSpan(getShop, func(span opentracing.Span) http.HandlerFunc {
+		return parseNpcId(l, func(npcId uint32) http.HandlerFunc {
+			return handleGetShop(l, db)(span)(npcId)
+		})
+	})
+}
 
-			if len(items) == 0 {
-				w.WriteHeader(http.StatusNotFound)
-				return
-			}
+func handleGetShop(l logrus.FieldLogger, db *gorm.DB) func(span opentracing.Span) func(npcId uint32) http.HandlerFunc {
+	return func(span opentracing.Span) func(npcId uint32) http.HandlerFunc {
+		return func(npcId uint32) http.HandlerFunc {
+			return func(w http.ResponseWriter, r *http.Request) {
+				items, err := item.GetByShopId(db)(npcId)
+				if err != nil {
+					l.WithError(err).Errorf("Unable to get the shops items.")
+					w.WriteHeader(http.StatusInternalServerError)
+					return
+				}
 
-			is := make([]ItemAttributes, 0)
-			for _, i := range items {
-				is = append(is, ItemAttributes{
-					ItemId:   i.ItemId(),
-					Price:    i.Price(),
-					Pitch:    i.Pitch(),
-					Position: i.Position(),
-				})
-			}
+				if len(items) == 0 {
+					w.WriteHeader(http.StatusNotFound)
+					return
+				}
 
-			result := &DataContainer{
-				Data: DataBody{
-					Id:   "",
-					Type: "",
-					Attributes: Attributes{
-						NPC:   uint32(npcId),
-						Items: is,
+				is := make([]ItemAttributes, 0)
+				for _, i := range items {
+					is = append(is, ItemAttributes{
+						ItemId:   i.ItemId(),
+						Price:    i.Price(),
+						Pitch:    i.Pitch(),
+						Position: i.Position(),
+					})
+				}
+
+				result := &DataContainer{
+					Data: DataBody{
+						Id:   "",
+						Type: "",
+						Attributes: Attributes{
+							NPC:   npcId,
+							Items: is,
+						},
 					},
-				},
-			}
+				}
 
-			w.WriteHeader(http.StatusOK)
-			err = json.ToJSON(result, w)
-			if err != nil {
-				l.WithError(err).Errorf("Writing response.")
+				w.WriteHeader(http.StatusOK)
+				err = json.ToJSON(result, w)
+				if err != nil {
+					l.WithError(err).Errorf("Writing response.")
+				}
 			}
 		}
 	}
